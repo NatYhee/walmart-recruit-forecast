7aebe867d0d593ba35f75464e48c1fc7245f2f8b
npanj <nat.panjar@gmail.com>
2022-08-11 16:49:31 +0700

    chore: initial utils script and create function to help prepare data for training and testing

---
+++ utils/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def date_setting(df: pd.DataFrame, date_col: str = "Date"):
+    df[date_col] = pd.to_datetime(df[date_col])
+    df["week"] = df[date_col].dt.isocalendar().week
+    df["month"] = df[date_col].dt.month
+    df["year"] = df[date_col].dt.year
+
+    df = df.sort_values(by=["Store", date_col], ascending=True)
+    df.set_index(date_col, inplace=True)
+    return df
