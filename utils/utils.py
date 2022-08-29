9af7a3ba2d44d65a254a2779dc539285a084e66c
npanj <nat.panjar@gmail.com>
2022-08-29 07:44:37 +0700

    refactor: reformat code

--- utils/utils.py
+++ utils/utils.py
@@ -9,4 +9,5 @@ def date_setting(df: pd.DataFrame, date_col: str = "Date"):
 
     df = df.sort_values(by=["Store", date_col], ascending=True)
     df.set_index(date_col, inplace=True, drop=False)
+    df.rename(columns={"Date": "date"}, inplace=True)
     return df
